560445aba3db5b66dd50c911e1ec32c5a3e7df43
felipezardo <zetazarudo@proton.me>
2025-08-02 18:11:41 -0300

    menu toggle adjustments

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -47,3 +47,11 @@ async function loadAttractions() {
 }
 
 loadAttractions();
+
+//  Mobile menu toggle 
+const menuButton = document.getElementById('menu');
+const nav = document.querySelector('.navigation');
+
+menuButton?.addEventListener('click', () => {
+  nav.classList.toggle('open');
+});
